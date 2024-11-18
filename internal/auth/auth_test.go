2dc8b46418b03bafb2eed7cb65695fab95e265d8
chaeanthony <chaeanthony21@gmail.com>
2024-11-18 10:11:34 -0500

    fmt files to pass fmt job

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,22 +8,22 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-	tests := map[string]struct{
-		input http.Header
+	tests := map[string]struct {
+		input   http.Header
 		wantRes string
 		wantErr error
 	}{
-		"valid": {input: http.Header{"Authorization": []string{"ApiKey api_key"}, "Content-Type":[]string{"application/json"}}, wantRes: "api_key", wantErr: nil},
+		"valid":                      {input: http.Header{"Authorization": []string{"ApiKey api_key"}, "Content-Type": []string{"application/json"}}, wantRes: "api_key", wantErr: nil},
 		"invalid auth header format": {input: http.Header{"Authorization": []string{"bad api_key"}}, wantRes: "", wantErr: errors.New("malformed authorization header")},
-		"no auth header": {input: http.Header{"Content-Type":[]string{"application/json"}}, wantRes: "", wantErr: errors.New("no authorization header included")},
+		"no auth header":             {input: http.Header{"Content-Type": []string{"application/json"}}, wantRes: "", wantErr: errors.New("no authorization header included")},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-				got, err := GetAPIKey(tc.input)
-				if !reflect.DeepEqual(tc.wantRes, got) {
-					t.Fatalf("expected: %v, got: %v\nexpected error: %v\nerror: %v", tc.wantRes, got, tc.wantErr, err)
-				}
+			got, err := GetAPIKey(tc.input)
+			if !reflect.DeepEqual(tc.wantRes, got) {
+				t.Fatalf("expected: %v, got: %v\nexpected error: %v\nerror: %v", tc.wantRes, got, tc.wantErr, err)
+			}
 		})
 	}
 
