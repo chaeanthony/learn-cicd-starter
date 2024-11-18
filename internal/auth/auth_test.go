5982e86815a0da1134dcfd0c28e97fb5247461d6
chaeanthony <chaeanthony21@gmail.com>
2024-11-17 20:27:16 -0500

    add auth_test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := map[string]struct{
+		input http.Header
+		wantRes string
+		wantErr error
+	}{
+		"valid": {input: http.Header{"Authorization": []string{"ApiKey api_key"}, "Content-Type":[]string{"application/json"}}, wantRes: "api_key", wantErr: nil},
+		"invalid auth header format": {input: http.Header{"Authorization": []string{"bad api_key"}}, wantRes: "", wantErr: errors.New("malformed authorization header")},
+		"no auth header": {input: http.Header{"Content-Type":[]string{"application/json"}}, wantRes: "", wantErr: errors.New("no authorization header included")},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+				got, err := GetAPIKey(tc.input)
+				if !reflect.DeepEqual(tc.wantRes, got) {
+					t.Fatalf("expected: %v, got: %v\nexpected error: %v\nerror: %v", tc.wantRes, got, tc.wantErr, err)
+				}
+		})
+	}
+
+}
